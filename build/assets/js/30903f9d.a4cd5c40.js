e52325f237bcd4c1e8acde4589e7ddee870064b6
yassinebensalem <49484211+yassinebensalem@users.noreply.github.com>
2023-03-15 12:13:22 +0100

    Delete build directory

--- build/assets/js/30903f9d.a4cd5c40.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkz_4_c=self.webpackChunkz_4_c||[]).push([[2141],{5745:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-pages","id":"default"}')}}]);
--- build/assets/js/a2b71213.098d5971.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkz_4_c=self.webpackChunkz_4_c||[]).push([[9976],{3769:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);
